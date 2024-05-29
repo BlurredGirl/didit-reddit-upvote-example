7e199b604602185b1937789d78df00ae5f0407cb
BlurredGirl <163426219+BlurredGirl@users.noreply.github.com>
2024-05-29 12:26:07 +0100

    please do not break it

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,11 @@
-import { handlers } from "@/auth";
+// import { handlers } from "@/auth";
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+export const { handlers, auth } = NextAuth(req => {
+ if (req) {
+  console.log(req) // do something with the request
+ }
+ return { providers: [ GitHub ] }
+})
 
 export const { GET, POST } = handlers;
